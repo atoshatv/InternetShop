a4516e5c7f1ca864dd4a42a0e74937f39119b7fe
Svetlichny-Evgen <svetlichnyi.e@gmail.com>
2023-12-19 23:51:30 +0200

    I added a product model, created ProductCard and ProductArea elements

---
+++ InternetShop/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace InternetShop.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+    }
+}
