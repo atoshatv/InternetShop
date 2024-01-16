f5d3d531b6a1badf34278f2a07f36b1953d604c6
Svetlichny-Evgen <svetlichnyi.e@gmail.com>
2024-01-16 17:28:33 +0200

    Created test items and added a prototype of the hover over picture function

--- InternetShop/Models/Product.cs
+++ InternetShop/Models/Product.cs
@@ -6,5 +6,16 @@
         public string[] Photos { get; set; }
         public string Title { get; set; }
         public decimal Price { get; set; }
+        public Product()
+        {
+            
+        }
+        public Product(Guid id, string[] photos, string title, decimal price)
+        {
+            Id = id;
+            Photos = photos;
+            Title = title;
+            Price = price;
+        }
     }
 }
