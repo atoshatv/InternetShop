718c60325ed7984e5aa45353e9f2d2953bdfaedf
ant7tv@gmail.com <ant7tv@gmail.com>
2024-01-24 18:11:07 +0100

    Slider besser

--- InternetShop/wwwroot/js/Slider.js
+++ InternetShop/wwwroot/js/Slider.js
@@ -1,5 +1,8 @@
 ﻿window.ScrollSlider = function (slider, current) {
     
     let width = slider.offsetWidth;
-    slider.scroll({left: current * width, behavior:'smooth'})
+    slider.scroll({ left: current * width, behavior: 'smooth' })
+
+  
 }
+
